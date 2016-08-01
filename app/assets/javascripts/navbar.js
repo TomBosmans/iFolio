fd8a810d0a53ad5082176e216d6eacc76b0ba3ab
Wolve <t.bosse@hotmail.com>
2016-08-01 13:21:49 +0000

    Header

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,9 +3,11 @@ $(window).on("scroll",function(){
   var height = parseInt($("#header").css("height"), 10);
   var value = Math.round($(window).scrollTop());
   if (value >= height) {
+    $("#header-content").addClass("hidden");
     $("#navbar").removeClass("hidden");
     $("#content").css("padding-top", "75px");
   } else {
+    $("#header-content").removeClass("hidden");
     $("#navbar").addClass("hidden");
     $("#content").css("padding-top", "0px");
   }
