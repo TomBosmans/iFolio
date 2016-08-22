59b8aeaac0877fefe30d81c803ec7920dd1e5c01
Wolve <t.bosse@hotmail.com>
2016-08-22 13:55:38 +0000

    Scrollspy generate nav for tinymce content

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,6 +1,6 @@
 // Using Masonry on articles
 function initialiceMasonry(){
-    $(function() {
+  $(function() {
     $('#articles').masonry({
       itemSelector: '.box',
       isFitWidth: true
@@ -9,7 +9,15 @@ function initialiceMasonry(){
 }
 
 function initialiceScrollspy(){
-  $('#article-content').scrollspy({ target: '#navbar-article' })
+  var headers = $('#article-content h1').get();
+  for (var i = 0; i < headers.length; i++) {
+    var id = "header-"+i
+    headers[i].setAttribute("id", id);
+    console.log(headers[i]);
+    $("#navbar-article ul").append('<li class="nav-item"><a class="nav-link" href="#'+id+'" data-turbolinks="false">'+ headers[i].innerHTML +'</a></li>');
+  }
+  
+   $('#article-content').scrollspy({ target: '#navbar-article' })
 }
 
 $(document).on('turbolinks:load', function() {
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160820134006) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
@@ -56,10 +59,9 @@ ActiveRecord::Schema.define(version: 20160820134006) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "profile_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
