815c9d7375f62c420cdc5e3fdf2ab8e64144aada
Wolve <t.bosse@hotmail.com>
2016-09-29 18:37:04 +0000

    layout

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -3,7 +3,7 @@ $(window).on("scroll",function(){
   var value = Math.round($(window).scrollTop());
   if (value >= height) {
     $("#profile-navbar").addClass("navbar-fixed-top");
-    $("#profile-navbar").css("margin-top", "77px");
+    $("#profile-navbar").css("margin-top", "54px");
     $("#content").css("padding-top", "200px");
     $("#profile-navbar-picture img").css("margin-top", "-11px");
     $("#profile-navbar-picture img").css("width", "70px");
@@ -12,7 +12,7 @@ $(window).on("scroll",function(){
   } else {
     $("#profile-navbar").removeClass("navbar-fixed-top");
     $("#profile-navbar").css("margin-top", "0px");
-    $("#content").css("padding-top", "77px");
+    $("#content").css("padding-top", "54px");
     $("#profile-navbar-picture img").css("margin-top", "-125px");
     $("#profile-navbar-picture img").css("width", "200px");
     $("#profile-navbar-picture img").css("height", "200px");
